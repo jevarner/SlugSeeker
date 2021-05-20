a1f85d88a455b3146d8ce6432f73ffee6bb42908
jevarner <jacob.varner0@gmail.com>
2021-05-19 21:05:17 -0400

    Add project files.

---
+++ SlugSeeker/Controllers/NewsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SlugSeeker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NewsController : ControllerBase
+    {
+        private readonly ILogger<NewsController> _logger;
+
+        public NewsController(ILogger<NewsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public static void GetStories()
+        {
+            var url = 
+        }
+    }
+}
